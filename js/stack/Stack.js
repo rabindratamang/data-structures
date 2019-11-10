39f5397a9457d081da24c563b55e5adb36e4f826
rabindratamang <rabindra.tamang@yarshastudio.com>
2019-11-10 23:17:58 +0545

    Add function documentation

--- js/stack/Stack.js
+++ js/stack/Stack.js
@@ -1,10 +1,18 @@
+/**
+ * Stack constructor
+ * @param {number} capacity
+ */
 function Stack(capacity){
     this.stack = [];
-    this.top = -1;
+    this.top = -1;//initially top is set -1 representing empty stack
     this.capacity = capacity;
-    //initially top is set -1 representing empty stack
 }
 
+/**
+ * Pushes value to the Stack
+ * 
+ * @param {number} value
+ */
 Stack.prototype.push = function(value){
     if(this.top === this.capacity-1){
         console.log('Stack overflow');
@@ -13,6 +21,11 @@ Stack.prototype.push = function(value){
     }
 }
 
+/**
+ * Pops value from the Stack
+ * 
+ * @returns
+ */
 Stack.prototype.pop = function(){
     if(this.isEmpty()){
         console.log('Stack is empty.')
@@ -22,6 +35,11 @@ Stack.prototype.pop = function(){
 
 }
 
+/**
+ * Returns value at top of stack
+ * 
+ * @returns
+ */
 Stack.prototype.peek = function(){
     if(this.isEmpty()){
         console.log('Stack is empty.')
