db4615715d5bfcb91d34365bb344d306e0b962a7
rabindratamang <rabindra.tamang@yarshastudio.com>
2019-11-16 21:53:40 +0545

    minor fix

--- js/algorithm/MergeSort.js
+++ js/algorithm/MergeSort.js
@@ -1,10 +1,17 @@
-function MergeSort(arr = []){
+function merge(left,right){
+    console.log(left)
+    console.log(right)    
+}
+
+function mergeSort(arr = []){
     if(arr.length<2) return arr;
     let middle = Math.floor(arr.length/2);
+    console.log(middle)
     let left = arr.slice(0,middle)
-    let right = arr.slice(middle+1,arr.length - 1);
+    let right = arr.slice(middle);
+    return merge(mergeSort(left),mergeSort(right));
     console.log(left)
     console.log(right)
 }
 
-MergeSort([1,4,5,3,2])
+mergeSort([1,4,5,3,2])
