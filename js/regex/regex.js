9f3217e05841d5153a7ba1dd192e7a30d4bcd586
Rabindra Tamang <rabindratamang147@gmail.com>
2019-11-19 22:28:18 +0545

    Update regex.js

--- js/regex/regex.js
+++ js/regex/regex.js
@@ -11,3 +11,20 @@ console.log(match)
 //. any character
 //* 0 or more
 //.* any number or characher more
+
+//single Char
+// \d
+// \w a-z A-Z 0-9 \W - 'not'
+// \s white space
+
+//quantifier
+// * - 0 or more characters
+// + - one or more characters
+// ? - 0 or 1 characher ? optionally  
+// {min,max}
+// {n} - \w{5} finds all 5 lettter words 
+
+//position
+// ^ begining
+// $ last
+// \b wordbounding
