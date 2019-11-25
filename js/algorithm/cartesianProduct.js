5dbec0f9c4440ff584e9e7431177755eaccababa
Rabindra Tamang <rabindratamang147@gmail.com>
2019-11-25 22:22:44 +0545

    Add Cartesian Product

---
+++ js/algorithm/cartesianProduct.js
@@ -0,0 +1,17 @@
+function cartesianProduct(setA, setB) {
+    if (!setA || !setB || !setA.length || !setB.length) {
+      return null;
+    }
+  
+    const product = [];
+  
+    for (let indexA = 0; indexA < setA.length; indexA += 1) {
+      for (let indexB = 0; indexB < setB.length; indexB += 1) {
+        product.push([setA[indexA], setB[indexB]]);
+      }
+    }
+  
+    return product;
+}
+
+console.log(cartesianProduct([1,2,3],[2,4,5]));
